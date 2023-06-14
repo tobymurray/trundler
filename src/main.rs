d44b9f59b166eb30bfc4d35b22189382d916e34b
Toby Murray <toby.murray@protonmail.com>
2023-06-14 12:01:40 -0400

    Add webhook get query

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use graphql_client::{GraphQLQuery, Response};
 use reqwest::header;
 use std::env;
 
-use crate::queries::{webhook_subscriptions, WebhookSubscriptions};
+use crate::queries::{webhook_subscription, webhook_subscriptions, WebhookSubscription, WebhookSubscriptions};
 
 pub const APPLICATION_GRAPHQL: &str = "application/graphql";
 pub const SHOPIFY_ACCESS_TOKEN: &str = "X-Shopify-Access-Token";
@@ -13,20 +13,31 @@ pub const SHOPIFY_ACCESS_TOKEN: &str = "X-Shopify-Access-Token";
 async fn main() -> Result<(), reqwest::Error> {
 	let api_key = env::var("SHOPIFY_API_KEY").unwrap();
 	let client = reqwest::Client::new();
+
 	let graphql_endpoint = graphql_endpoint_for(
 		"toby-test-store2.myshopify.com".strip_suffix(".myshopify.com").unwrap(),
 		"2023-04",
 	);
 
+	let id = "gid://shopify/WebhookSubscription/1166233927878".to_string();
+
 	let request = client
 		.post(graphql_endpoint)
 		.header(header::CONTENT_TYPE, APPLICATION_GRAPHQL)
 		.header(SHOPIFY_ACCESS_TOKEN, api_key)
-		.json(&WebhookSubscriptions::build_query(webhook_subscriptions::Variables {}));
+		.json(&WebhookSubscription::build_query(webhook_subscription::Variables {
+			id,
+		}));
+
+	// let request = client
+	// 	.post(graphql_endpoint)
+	// 	.header(header::CONTENT_TYPE, APPLICATION_GRAPHQL)
+	// 	.header(SHOPIFY_ACCESS_TOKEN, api_key)
+	// 	.json(&WebhookSubscriptions::build_query(webhook_subscriptions::Variables {}));
 
 	let raw_response = request.send().await?;
 
-	let response: Response<webhook_subscriptions::ResponseData> = raw_response.json().await?;
+	let response: Response<webhook_subscription::ResponseData> = raw_response.json().await?;
 	println!("{:#?}", response);
 	Ok(())
 }
--- src/queries.rs
+++ src/queries.rs
@@ -2,10 +2,22 @@ use graphql_client::GraphQLQuery;
 
 pub type DateTime = chrono::DateTime<chrono::Utc>;
 
+// Has to match the generated type's name
+#[allow(clippy::upper_case_acronyms)]
+pub type URL = String;
+
 #[derive(GraphQLQuery)]
 #[graphql(
 	schema_path = "2023-04.json",
-	query_path = "graphql-queries/webhook_subscriptions.graphql",
+	query_path = "graphql-queries/webhooks/subscriptions.graphql",
 	response_derives = "Debug"
 )]
 pub struct WebhookSubscriptions;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+	schema_path = "2023-04.json",
+	query_path = "graphql-queries/webhooks/get.graphql",
+	response_derives = "Debug"
+)]
+pub struct WebhookSubscription;
